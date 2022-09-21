e0b91f538ff013bdf3c960bbf6cd53a5c2468269
YeimerSebastianRomeroGilADSO <chattian369@gmail.com>
2022-09-21 11:36:46 -0500

    punto 4

--- main.js
+++ main.js
@@ -1,10 +1,8 @@
 addEventListener("DOMContentLoaded", (e)=>{
-    let num1=Number(prompt("ingrese el primer numero "))
-    let num2=Number(prompt("ingrese el segundo numero "))
-    let num3=Number(prompt("ingrese el tercer numero "))
-    let num4=Number(prompt("ingrese el cuarto numero "))
-    let op=(num1+num2+num3+num4)/4
-    alert(`la media es: ${op}`)
+    let base=Number(prompt("ingrese el primer numero "))
+    let altura=Number(prompt("ingrese el segundo numero "))
+    let op=(base*altura)/2
+    alert(`area de el triangulo: ${op}`)
     
 
 })
