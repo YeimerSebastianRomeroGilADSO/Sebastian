53dde24990988969be0b462b82076c8adcd3ab46
YeimerSebastianRomeroGilADSO <chattian369@gmail.com>
2022-09-21 11:30:40 -0500

    punto 3

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 addEventListener("DOMContentLoaded", (e)=>{
-    let lado1=3
-    let lado2=4
-    let op=lado1*lado2
-    alert(`EL RESULTADO ES :${op}`)
+    let num1=Number(prompt("ingrese el primer numero "))
+    let num2=Number(prompt("ingrese el segundo numero "))
+    let num3=Number(prompt("ingrese el tercer numero "))
+    let num4=Number(prompt("ingrese el cuarto numero "))
+    let op=(num1+num2+num3+num4)/4
+    alert(`la media es: ${op}`)
+    
 
 })
