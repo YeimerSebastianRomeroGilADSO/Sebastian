7649dcad68e709e8c79f6e51248a0af3c865afca
YeimerSebastianRomeroGilADSO <chattian369@gmail.com>
2022-09-21 11:20:31 -0500

    punto 2

--- main.js
+++ main.js
@@ -1,13 +1,7 @@
 addEventListener("DOMContentLoaded", (e)=>{
-    let num1=Number(prompt("ingrese el primer numero"))
-    let num2=Number(prompt("ingrese el segundo numero"))
-    let suma=num1+num2
-    let resta=num1-num2
-    let multi=num1*num2
-    let div=num1/num2
-    alert(`el resultado de su operacion es :${suma}`)
-    alert(`el resultado de su operacion es :${resta}`)
-    alert(`el resultado de su operacion es :${multi}`)
-    alert(`el resultado de su operacion es :${div}`)
+    let lado1=3
+    let lado2=4
+    let op=lado1*lado2
+    alert(`EL RESULTADO ES :${op}`)
 
 })
