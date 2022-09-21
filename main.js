ec433e1cb1a29dc84c20f9072d22e021c994ccda
YeimerSebastianRomeroGilADSO <chattian369@gmail.com>
2022-09-21 11:04:39 -0500

    primer punto

---
+++ main.js
@@ -0,0 +1,13 @@
+addEventListener("DOMContentLoaded", (e)=>{
+    let num1=Number(prompt("ingrese el primer numero"))
+    let num2=Number(prompt("ingrese el segundo numero"))
+    let suma=num1+num2
+    let resta=num1-num2
+    let multi=num1*num2
+    let div=num1/num2
+    alert(`el resultado de su operacion es :${suma}`)
+    alert(`el resultado de su operacion es :${resta}`)
+    alert(`el resultado de su operacion es :${multi}`)
+    alert(`el resultado de su operacion es :${div}`)
+
+})
